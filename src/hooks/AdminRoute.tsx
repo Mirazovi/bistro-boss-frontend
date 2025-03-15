eca1b64bfad4ead5b6182f94ec29c50831201463
Mirazovi <mdmirazhossainovi@gmail.com>
2025-03-15 11:25:55 +0600

    add item

---
+++ src/hooks/AdminRoute.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import useAdmin from "./useAdmin"
+import { AuthContext } from "../firebase/AuthProvider";
+import { Navigate, useLocation } from "react-router";
+
+const AdminRoute = ({children}) => {
+    const [isAdmin,isAdminLoading] = useAdmin();
+    const {user,loading} = useContext(AuthContext)
+    const location = useLocation()
+    if(loading || isAdminLoading){
+        return <span className="loading loading-spinner loading-lg"></span>
+    }
+    if(user && isAdmin){
+       return children;
+    }
+  return <Navigate to={'/login'} state={{from:location}} replace></Navigate>
+}
+
+export default AdminRoute
